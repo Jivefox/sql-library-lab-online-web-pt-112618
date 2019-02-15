08a69d35f911355bcbc4c48f1c40777e79632ec6
Cliff Jurgensen <jivefox.002@gmail.com>
2019-02-15 19:18:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,9 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN characters
   ON character.series_id = series.id
-  WHERE character.species_id = 'human'"
+  WHERE character.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT()"
 end
 
 def select_character_names_and_number_of_books_they_are_in
